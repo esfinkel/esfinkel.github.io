d2168e21be37af82a96ab6bcee003872141d16f7
Elisabeth Finkel <32049677+esfinkel@users.noreply.github.com>
2019-08-11 16:18:55 -0500

    Updates

---
+++ precache-manifest.3132e8ef968e2b82b7074da865f4b8c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e7bdc98fdd4d840dd61b4dfb244c39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69abc43cb2a7e713cfef",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0802f01ee64a4b1bb55d",
+    "url": "/static/js/2.b7084f1a.chunk.js"
+  },
+  {
+    "revision": "69abc43cb2a7e713cfef",
+    "url": "/static/js/main.b0ae5411.chunk.js"
+  },
+  {
+    "revision": "338270fb3720bf7e1cd6",
+    "url": "/static/js/runtime~main.ace06e6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
